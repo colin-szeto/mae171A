7d9e73fff4911c91b309d66db93d67a45633c394
unknown <kolinszeto@gmail.com>
2025-02-14 10:34:10 -0800

    lab2

--- lab2/from class/parameters.m
+++ lab2/from class/parameters.m
@@ -4,11 +4,15 @@
 % Written by R.A. de Callafon, Dept. of MAE, UCSD (2001-2021)
 % Report errors in this software to <callafon@ucsd.edu>
 
-m1=  3.2539e-07;         % Mass m1                     
-d1=   1.7831e-06;         % Damping that connects m1 to ground  
-k1=   1.7539e-04;         % Spring that connect m1 to ground
+%m1=  3.2539e-07;         % Mass m1    
+m1=  0.91*3.2539e-07;         % Mass m1                     
 
-m2=  6.7591e-07;         % Mass m2                     
+d1=   .91*1.7831e-06;         % Damping that connects m1 to ground  
+k1=   .91*1.7539e-04;         % Spring that connect m1 to ground
+
+%m2=  6.7591e-07;         % Mass m2                
+m2=  0.9*6.7591e-07;         % Mass m2                     
+
 d2=  1.4510e-06;         % Damping that connects m2 to ground 
 k2=  1.6325e-04;      % Spring that connects m1 and m2      
 
--- lab2/from class/trim_.m
+++ lab2/from class/trim_.m
@@ -1,6 +1,13 @@
 clear; close all; clc; 
 
 load('test_2_07-Feb-2025 11_21_31.mat')
+load('07-Feb-2025 11_22_22.mat')
+%load('07-Feb-2025 11_23_51.mat')
+%load('07-Feb-2025 11_24_33.mat')
+%load('07-Feb-2025 11_25_10.mat')
 
-
-enc1_trim = enc1(time>5);
+enc1 = enc1(time>=5);
+enc2 = enc2(time>=5);
+duty = duty(time>=5);
+target_v = target_v(time>=5);
+time = time(time>=5)-5;
